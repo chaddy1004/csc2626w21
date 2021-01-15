4e930c37ea440862b281ba348de9453d2381c49b
chaddy <cpaik1004@gmail.com>
2021-01-15 18:56:50 -0500

    start on network

--- assignments/A1/racer.py
+++ assignments/A1/racer.py
@@ -10,30 +10,30 @@ import os
 from driving_policy import DiscreteDrivingPolicy
 from utils import DEVICE, str2bool
 
+
 def run(steering_network, args):
-    
     env = FullStateCarRacingEnv()
     env.reset()
-    
+
     learner_action = np.array([0.0, 0.0, 0.0])
     expert_action = None
-    
+
     for t in range(args.timesteps):
         env.render()
-        
-        state, expert_action, reward, done, _ = env.step(learner_action) 
+
+        state, expert_action, reward, done, _ = env.step(learner_action)
         if done:
             break
-        
+
         expert_steer = expert_action[0]  # [-1, 1]
-        expert_gas = expert_action[1]    # [0, 1]
+        expert_gas = expert_action[1]  # [0, 1]
         expert_brake = expert_action[2]  # [0, 1]
 
         if args.expert_drives:
             learner_action[0] = expert_steer
         else:
             learner_action[0] = steering_network.eval(state, device=DEVICE)
-            
+
         learner_action[1] = expert_gas
         learner_action[2] = expert_brake
 
@@ -42,24 +42,28 @@ def run(steering_network, args):
 
     env.close()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--out_dir", help="directory in which to save the expert's data", default='./dataset/train')
-    parser.add_argument("--save_expert_actions", type=str2bool, help="save the images and expert actions in the training set",
+    parser.add_argument("--save_expert_actions", type=str2bool,
+                        help="save the images and expert actions in the training set",
                         default=False)
-    
+
     parser.add_argument("--expert_drives", type=str2bool, help="should the expert steer the vehicle?", default=False)
-    parser.add_argument("--run_id", type=int, help="Id for this particular data collection run (e.g. dagger iterations)", default=0)
-    parser.add_argument("--timesteps", type=int, help="timesteps of simulation to run, up to one full loop of the track", default=100000)
-    parser.add_argument("--learner_weights", type=str, help="filename from which to load learner weights for the steering network",
+    parser.add_argument("--run_id", type=int,
+                        help="Id for this particular data collection run (e.g. dagger iterations)", default=0)
+    parser.add_argument("--timesteps", type=int,
+                        help="timesteps of simulation to run, up to one full loop of the track", default=100000)
+    parser.add_argument("--learner_weights", type=str,
+                        help="filename from which to load learner weights for the steering network",
                         default='')
     parser.add_argument("--n_steering_classes", type=int, help="number of steering classes", default=20)
-    
+
     args = parser.parse_args()
-    
+
     steering_network = DiscreteDrivingPolicy(n_classes=args.n_steering_classes).to(DEVICE)
     if args.learner_weights:
         steering_network.load_weights_from(args.learner_weights)
 
     run(steering_network, args)
-
--- assignments/A1/train_policy.py
+++ assignments/A1/train_policy.py
@@ -1,16 +1,44 @@
+import argparse
+import time
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
-from torchvision import datasets, transforms
-from torch.utils.data import Dataset, DataLoader
-import argparse
-import time
-import os
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
 from dataset_loader import DrivingDataset
 from driving_policy import DiscreteDrivingPolicy
 from utils import DEVICE, str2bool
+
+
+class Network(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        self.img_size = 128
+        self.conv24 = nn.Conv2d(in_channels=3, out_channels=24, kernel_size=4, stride=2, padding=1)
+        self.conv36 = nn.Conv2d(in_channels=24, out_channels=36, kernel_size=4, stride=2, padding=1)
+        self.conv48 = nn.Conv2d(in_channels=36, out_channels=48, kernel_size=4, stride=2, padding=1)
+        self.conv64 = nn.Conv2d(in_channels=48, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.flatten = nn.Flatten()
+
+        self.fc_128 = nn.Linear(in_features=4096, out_features=128)
+        self.fc_64 = nn.Linear(in_features=128, out_features=64)
+        self.fc_n_classes = nn.Linear(in_features=64, out_features=n_classes)
+
+        self.relu = nn.ReLU()
+
+        self.conv_layer = nn.Sequential(self.conv24, self.relu, self.conv36, self.relu, self.conv48, self.relu,
+                                        self.conv64)
+        self.fc_layer = nn.Sequential(self.fc_128, self.relu, self.fc_64, self.relu, self.fc_n_classes)
+
+    def forward(self, x):
+        conv_output = self.conv_layer(x)
+        conv_output_flattened = self.flatten(conv_output)
+        output = self.fc_layer(conv_output_flattened)
+        return output
+
 
 def train_discrete(model, iterator, opt, args):
     model.train()
@@ -23,19 +51,19 @@ def train_discrete(model, iterator, opt, args):
     # Compute the cross_entropy loss with and without weights  
     # Compute the derivatives of the loss w.r.t. network parameters
     # Take a step in the approximate gradient direction using the optimizer opt  
-    
+
     for i_batch, batch in enumerate(iterator):
 
         #
         # YOUR CODE GOES HERE
         #
-        
+
         loss = loss.detach().cpu().numpy()
         loss_hist.append(loss)
-        
-        PRINT_INTERVAL = int(len(iterator) / 3)        
+
+        PRINT_INTERVAL = int(len(iterator) / 3)
         if (i_batch + 1) % PRINT_INTERVAL == 0:
-            print ('\tIter [{}/{} ({:.0f}%)]\tLoss: {}\t Time: {:10.3f}'.format(
+            print('\tIter [{}/{} ({:.0f}%)]\tLoss: {}\t Time: {:10.3f}'.format(
                 i_batch, len(iterator),
                 i_batch / len(iterator) * 100,
                 np.asarray(loss_hist)[-PRINT_INTERVAL:].mean(0),
@@ -46,95 +74,94 @@ def train_discrete(model, iterator, opt, args):
 def accuracy(y_pred, y_true):
     "y_true is (batch_size) and y_pred is (batch_size, K)"
     _, y_max_pred = y_pred.max(1)
-    correct = ((y_true == y_max_pred).float()).mean() 
+    correct = ((y_true == y_max_pred).float()).mean()
     acc = correct * 100
     return acc
 
 
 def test_discrete(model, iterator, opt, args):
     model.train()
-    
+
     acc_hist = []
-    
+
     for i_batch, batch in enumerate(iterator):
         x = batch['image']
         y = batch['cmd']
 
         x = x.to(DEVICE)
         y = y.to(DEVICE)
-        
+
         logits = model(x)
         y_pred = F.softmax(logits, 1)
 
         acc = accuracy(y_pred, y)
         acc = acc.detach().cpu().numpy()
         acc_hist.append(acc)
-        
+
     avg_acc = np.asarray(acc_hist).mean()
-    
-    print ('\tVal: \tAcc: {}  Time: {:10.3f}'.format(
+
+    print('\tVal: \tAcc: {}  Time: {:10.3f}'.format(
         avg_acc,
         time.time() - args.start_time,
     ))
-    
+
     return avg_acc
-    
+
+
 def get_class_distribution(iterator, args):
     class_dist = np.zeros((args.n_steering_classes,), dtype=np.float32)
     for i_batch, batch in enumerate(iterator):
         y = batch['cmd'].detach().numpy().astype(np.int32)
         class_dist[y] += 1
-        
+
     return (class_dist / sum(class_dist))
 
-    
+
 def main(args):
-    
-    data_transform = transforms.Compose([ transforms.ToPILImage(),
-                                          transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-                                          transforms.RandomRotation(degrees=80),
-                                          transforms.ToTensor()])
-    
+    data_transform = transforms.Compose([transforms.ToPILImage(),
+                                         transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+                                         transforms.RandomRotation(degrees=80),
+                                         transforms.ToTensor()])
+
     training_dataset = DrivingDataset(root_dir=args.train_dir,
                                       categorical=True,
                                       classes=args.n_steering_classes,
                                       transform=data_transform)
-    
+
     validation_dataset = DrivingDataset(root_dir=args.validation_dir,
                                         categorical=True,
                                         classes=args.n_steering_classes,
                                         transform=data_transform)
-    
+
     training_iterator = DataLoader(training_dataset, batch_size=args.batch_size, shuffle=True, num_workers=1)
     validation_iterator = DataLoader(validation_dataset, batch_size=args.batch_size, shuffle=True, num_workers=1)
     driving_policy = DiscreteDrivingPolicy(n_classes=args.n_steering_classes).to(DEVICE)
-    
+
     opt = torch.optim.Adam(driving_policy.parameters(), lr=args.lr)
     args.start_time = time.time()
-    
-    print (driving_policy)
-    print (opt)
-    print (args)
+
+    print(driving_policy)
+    print(opt)
+    print(args)
 
     args.class_dist = get_class_distribution(training_iterator, args)
 
-    best_val_accuracy = 0 
+    best_val_accuracy = 0
     for epoch in range(args.n_epochs):
-        print ('EPOCH ', epoch)
+        print('EPOCH ', epoch)
 
         #
         # YOUR CODE GOES HERE
         #
-        
+
         # Train the driving policy
         # Evaluate the driving policy on the validation set
         # If the accuracy on the validation set is a new high then save the network weights 
-        
-        
+
     return driving_policy
 
+
 if __name__ == "__main__":
-    
     parser = argparse.ArgumentParser()
     parser.add_argument("--lr", type=float, help="learning rate", default=1e-3)
     parser.add_argument("--n_epochs", type=int, help="number of epochs", default=50)
@@ -142,14 +169,13 @@ if __name__ == "__main__":
     parser.add_argument("--n_steering_classes", type=int, help="number of steering classes", default=20)
     parser.add_argument("--train_dir", help="directory of training data", default='./dataset/train')
     parser.add_argument("--validation_dir", help="directory of validation data", default='./dataset/val')
-    parser.add_argument("--weights_out_file", help="where to save the weights of the network e.g. ./weights/learner_0.weights",
+    parser.add_argument("--weights_out_file",
+                        help="where to save the weights of the network e.g. ./weights/learner_0.weights",
                         required=True)
     parser.add_argument("--weighted_loss", type=str2bool,
                         help="should you weight the labeled examples differently based on their frequency of occurence",
                         default=False)
-    
+
     args = parser.parse_args()
 
     main(args)
-        
-    
