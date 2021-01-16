a2f475ad2f1a76bd8a3770799541201126fc5b67
chaddy <cpaik1004@gmail.com>
2021-01-16 15:21:52 -0500

    fix: implement policy network

--- assignments/A1/driving_policy.py
+++ assignments/A1/driving_policy.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import numpy as np
 from torchvision import datasets, transforms
 
+
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
@@ -13,61 +14,57 @@ def weights_init(m):
         except AttributeError:
             print("Skipping initialization of ", classname)
 
-            
+
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
-    
+
 
 class DiscreteDrivingPolicy(nn.Module):
 
     def __init__(self, n_classes):
         super().__init__()
         self.n_classes = n_classes
+        self.conv24 = nn.Conv2d(in_channels=3, out_channels=24, kernel_size=4, stride=2, padding=1)
+        self.conv36 = nn.Conv2d(in_channels=24, out_channels=36, kernel_size=4, stride=2, padding=1)
+        self.conv48 = nn.Conv2d(in_channels=36, out_channels=48, kernel_size=4, stride=2, padding=1)
+        self.conv64 = nn.Conv2d(in_channels=48, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.flatten = nn.Flatten()
 
-        
-        self.features = nn.Sequential(
-            #
-            # YOUR CODE GOES HERE
-            #
-            Flatten(),
-        )
-        
-        self.classifier = nn.Sequential(
-            #
-            # YOUR CODE GOES HERE
-            #
-        )  
-        
+        self.fc_128 = nn.Linear(in_features=4096, out_features=128)
+        self.fc_64 = nn.Linear(in_features=128, out_features=64)
+        self.fc_n_classes = nn.Linear(in_features=64, out_features=n_classes)
+
+        self.relu = nn.ReLU()
+
+        self.features = nn.Sequential(self.conv24, self.relu, self.conv36, self.relu, self.conv48, self.relu,
+                                      self.conv64, self.relu, self.flatten)
+        self.classifier = nn.Sequential(self.fc_128, self.relu, self.fc_64, self.relu, self.fc_n_classes)
+
         self.apply(weights_init)
-        
-        
+
     def forward(self, x):
         f = self.features(x)
         logits = self.classifier(f)
         return logits
-    
+
     def eval(self, state, device):
         state = state.astype(np.float32)
         state = np.ascontiguousarray(np.transpose(state, (2, 0, 1)))
         state = torch.tensor(state).to(torch.device(device))
         state = state.unsqueeze(0)
         logits = self.forward(state)
-        
-        y_pred = logits.view(-1, self.n_classes) 
+
+        y_pred = logits.view(-1, self.n_classes)
         y_probs_pred = F.softmax(y_pred, 1)
 
-        _, steering_class = torch.max(y_probs_pred, dim=1)
+        _, steering_class = torch.max(y_probs_pred, dim=1) # retuns (values, indices)
         steering_class = steering_class.detach().cpu().numpy()
-        
-        steering_cmd = (steering_class / (self.n_classes - 1.0))*2.0 - 1.0
-        
+
+        steering_cmd = (steering_class / (self.n_classes - 1.0)) * 2.0 - 1.0
+
         return steering_cmd
-    
+
     def load_weights_from(self, weights_filename):
         weights = torch.load(weights_filename)
-        self.load_state_dict( {k:v for k,v in weights.items()}, strict=True)
-
-    
-
-
+        self.load_state_dict({k: v for k, v in weights.items()}, strict=True)
